32ec8aa4be05194a34d185663321a5b3f72d1a72
pol-cova <83374367+pol-cova@users.noreply.github.com>
2023-05-18 08:57:22 -0600

    test exercise

---
+++ test.c
@@ -0,0 +1,7 @@
+//
+// Created by Paul Contreras on 18/05/23.
+//
+#include <stdio.h>
+
+int main(){int n, sum = 0; printf("Ingrese un numero"); scanf("%d", &n); for (int i = n+1; i<=n+10; i++){sum = sum + i;} if (n>= 1 && n<= 20){ printf("La suma es: %d", sum);} return 0;}
+
